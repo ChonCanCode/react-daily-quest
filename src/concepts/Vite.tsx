a3b13b9b698a250b7511575ff93aff4528cff455
Chon Lou <163328241+ChonCanCode@users.noreply.github.com>
2025-01-30 17:51:03 +0000

    update

---
+++ src/concepts/Vite.tsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+const Vite = () => {
+    return (
+        <>
+        <Link to="/">Home Page</Link>
+        <h1>Why Use Vite for a React Project?</h1>
+
+        <p>When creating a React project, we often use Create React App (CRA) or Vite. Vite is preferred for several reasons:</p>
+
+        <ol>
+            <li><strong>1. Super Fast Development Server </strong></li>
+            <ul>
+                <li></li>
+                <li></li>
+            </ul>
+
+
+        </ol>
+
+
+
+        </>
+    );
+};
+
+export default Vite;
