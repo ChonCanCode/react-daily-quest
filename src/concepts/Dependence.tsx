aced0c96f09956492c580deb3075c6fef22c295e
ChonCanCode <163328241+ChonCanCode@users.noreply.github.com>
2025-02-01 22:11:13 +0000

    update

--- src/concepts/Dependence.tsx
+++ src/concepts/Dependence.tsx
@@ -1,15 +1,13 @@
 import { Link } from "react-router-dom";
 
-
 const Dependence = () => {
-    return (
-      <>
-      <Link to="/">Home Page</Link> 
-       <h1>What is Dependence?</h1>
+  return (
+    <>
+      <Link to="/">Home Page</Link>
+      <h1>What is Dependence?</h1>
+      <p></p>
+    </>
+  );
+};
 
-      </>
-    );
-  };
-  
-  export default Dependence;
-  
+export default Dependence;
