f6a265a03af9551cfe5da2862b5a29fbf4bf0e2e
ChonCanCode <163328241+ChonCanCode@users.noreply.github.com>
2025-02-02 14:17:46 +0000

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
-import { Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import ChallengePage from './pages/ChallengePage';
-import Component from './concepts/Component';// Import the ComponentPage
-import State from './concepts/State';
-import Hook from './concepts/Hook';
-import TypeScript from './concepts/TypeScript';
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ChallengePage from "./pages/ChallengePage";
+import Component from "./concepts/Component"; // Import the ComponentPage
+import State from "./concepts/State";
+import Hook from "./concepts/Hook";
+import Dependence from "./concepts/Dependence";
+import TypeScript from "./concepts/TypeScript";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/challenge/:day" element={<ChallengePage />} />
-      <Route path="/Component" element={<Component />} /> {/* Add the new route */}
-      <Route path="/State" element={ <State />}/>
-      <Route path="/Hook" element={ <Hook />}/>
-      <Route path="/TypeScript" element={ <TypeScript />}/>
+      <Route path="/Component" element={<Component />} />{" "}
+      {/* Add the new route */}
+      <Route path="/State" element={<State />} />
+      <Route path="/Hook" element={<Hook />} />
+      <Route path="/TypeScript" element={<TypeScript />} />
+      <Route path="/Dependence" element={<Dependence />} />
     </Routes>
   );
 };
--- src/challenges/day0.tsx
+++ src/challenges/day0.tsx
@@ -1,23 +1,35 @@
 import { Link } from "react-router-dom";
 
-
 const Day0 = () => {
-    return (
-      <>
-        <Link to="/">Home Page</Link> 
-        <h1>React Concepts</h1>
-        <ul>
-          <li><Link to="/Component">What is Component?</Link></li>
-          <li><Link to="/State">What is State?</Link></li>
-          <li><Link to="/Hook">What is Hook?</Link></li>
-          <li><Link to="/Dependency">What is Dependency?</Link></li>
-          <li><Link to="/TypeScript">What is TypeScript?</Link></li>
-          <li><Link to="/Vite">What is Vite?</Link></li>
-          <li><Link to="/Vite">What is Props?</Link></li>
-        </ul>
-      </>
-    );
-  };
-  
-  export default Day0;
-  
+  return (
+    <>
+      <Link to="/">Home Page</Link>
+      <h1>React Concepts</h1>
+      <ul>
+        <li>
+          <Link to="/Component">What is Component?</Link>
+        </li>
+        <li>
+          <Link to="/State">What is State?</Link>
+        </li>
+        <li>
+          <Link to="/Hook">What is Hook?</Link>
+        </li>
+        <li>
+          <Link to="/Dependence">What is Dependence?</Link>
+        </li>
+        <li>
+          <Link to="/TypeScript">What is TypeScript?</Link>
+        </li>
+        <li>
+          <Link to="/Vite">What is Vite?</Link>
+        </li>
+        <li>
+          <Link to="/Vite">What is Props?</Link>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default Day0;
