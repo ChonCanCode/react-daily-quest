3fdff4d70bebb57f6f89a9c1ccb26453fdc7864c
Chon Lou <163328241+ChonCanCode@users.noreply.github.com>
2025-02-05 16:42:17 +0000

    Update day2.tsx

--- src/challenges/day2.tsx
+++ src/challenges/day2.tsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Day2 = () => {
+    
+    <>
+    <Link to="/">Home Page</Link>
+    </>
+    
 };
 
 export default Day2;
