57b74674984db667438acb693ab76fa30c8d4ea4
Chon Lou <163328241+ChonCanCode@users.noreply.github.com>
2025-02-07 16:01:56 +0000

    update mai

--- src/projects/StreetFighter6.tsx
+++ src/projects/StreetFighter6.tsx
@@ -8,6 +8,7 @@ const StreetFighter6 = () => {
       <h2>Game Plan</h2>
       <h3>Mid Screen</h3>
       <h4>Corner Carry</h4>
+
       <p>with Hissatse Shinobi Bachi (214K)</p>
       <ul>
         <li>2 ligthts &gt; 214LK &gt; -27F </li>
@@ -15,6 +16,44 @@ const StreetFighter6 = () => {
           <li>66 &gt; MP &gt; 2MP &gt; 236MKHK &gt; MP &gt; 623HK (2920 dmg)</li>
         </ul>
       </ul>
+
+      <p>with Fire Ball Game (214P)</p>
+      <ul>
+        <li>light</li>
+        <ul>
+          <li></li>
+        </ul>
+        <li>light (full charge)</li>
+        <ul>
+          <li></li>
+        </ul>
+        <li>Medium</li>
+        <ul>
+          <li></li>
+        </ul>
+        <li>Medium (full charge)</li>
+        <ul>
+          <li></li>
+        </ul>
+        <li>Heavy</li>
+        <ul>
+          <li></li>
+        </ul>
+        <li>Heavy (full charge)</li>
+        <ul>
+          <li></li>
+        </ul>
+        <li>OD</li>
+        <ul>
+          <li></li>
+        </ul>
+        <li>OD (full charge)</li>
+        <ul>
+          <li></li>
+        </ul>
+      </ul>
+
+
 
       <h3>Corner Pressure</h3>
       <h3>Corner Escape</h3>
